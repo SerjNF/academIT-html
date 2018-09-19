9c809260497a1cbe2159ebd3ed433ba550ed8974
SerjNF <foreman@inbox.ru>
2018-09-19 15:05:24 +0700

    Задачи по лекции 3

--- TODOList/TODO.js
+++ TODOList/TODO.js
@@ -2,19 +2,21 @@ document.addEventListener("DOMContentLoaded", function () {
 
     var count = 0;
     document.getElementById("enter-data").onclick = function () {
-        var fullName = document.getElementsByClassName("input");
         var table = document.createElement("table");
-
         table.className = "table-item";
 
         var tBody = document.createElement("tbody");
 
-        for (var i = 0; i < fullName.length; i++) {
-            var cell = document.createElement("td");
-            cell.className = "list-item";
-            cell.innerText = fullName[i].value;
-            tBody.appendChild(cell);
-        }
+        var fullName = document.getElementsByClassName("input");
+        var nameList  = Array.prototype.slice.call(fullName);
+
+        nameList.forEach(function (elem) {
+                var cell = document.createElement("td");
+                cell.className = "list-item";
+                cell.innerText = elem.value;
+                tBody.appendChild(cell);
+                elem.value = "";
+        });
 
         var delButton = document.createElement("input");
         delButton.type = "button";
