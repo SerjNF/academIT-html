af18bfcc33fb96d5161bd1d6e3dd28a873386e77
SerjNF <foreman@inbox.ru>
2018-09-25 13:37:21 +0700

    Замечания по JS

--- TODOList/TODO.js
+++ TODOList/TODO.js
@@ -19,27 +19,19 @@ document.addEventListener("DOMContentLoaded", function () {
         Element.prototype.closest = function (css) {
             var node = this;
             while (node) {
-                if (node.matches(css)) return node;
-                else node = node.parentElement;
+                if (node.matches(css)) {
+                    return node;
+                }
+                else {
+                    node = node.parentElement;
+                }
             }
             return null;
         };
     }
-    // строка таблицы
-    function createRows(nameList) {
-        var tr = document.createElement("tr");
-        var firstCell = document.createElement("td");
-        firstCell.className = "firstCell";
-        firstCell.id = "col1";
-        tr.appendChild(firstCell);
-        nameList.forEach(function (elem, index) {
-            var cell = document.createElement("td");
-            cell.className = "list-item";
-            cell.id = "col" + (index + 2);
-            cell.innerText = elem.value;
-            tr.appendChild(cell);
-            elem.value = "";
-        });
+
+    //кнопка удалить
+    function delButton() {
         var delButton = document.createElement("input");
         delButton.type = "button";
         delButton.value = "удалить";
@@ -47,9 +39,33 @@ document.addEventListener("DOMContentLoaded", function () {
             delButton.closest("tr").remove();
             reloadFirstCells();
         });
-        tr.appendChild(delButton);
+        return delButton;
+    }
+
+    // строка таблицы
+    function createRows(nameList) {
+        var tr = document.createElement("tr");
+        var firstCell = document.createElement("td");
+        firstCell.className = "col1";
+        firstCell.className = "firstCell";
+        tr.appendChild(firstCell);
+
+        nameList.forEach(function (elem, index) {
+            var cell = document.createElement("td");
+            cell.className = "list-item";
+            cell.className = "col" + (index + 2);
+            cell.innerText = elem.value;
+            tr.appendChild(cell);
+            elem.value = "";
+        });
+        var trLast = document.createElement("td");
+        trLast.className = "list-item";
+        trLast.className = "col5";
+        trLast.appendChild(delButton());
+        tr.appendChild(trLast);
         return tr;
     }
+
     // номера в списке
     function reloadFirstCells() {
         var firstCells = document.getElementsByClassName("firstCell");
@@ -58,6 +74,7 @@ document.addEventListener("DOMContentLoaded", function () {
             e.innerText = index + 1;
         });
     }
+
     // кнопка добавить
     document.getElementById("enter-data").addEventListener("click", function () {
         var fullName = document.getElementsByClassName("input");
